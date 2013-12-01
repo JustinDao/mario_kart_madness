0cde5413391715b0d1525d375b43fc50164fd81b
Justin Dao <tjd5at@virginia.edu>
2013-12-01 14:46:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarioKartMadness::Application.config.secret_key_base = 'bce36108c5241bcc284ec54c976d86ec9435b53cc9e805febc49c7bdb6552a9d2b485cdb090ad52da25011b9d169ee3bc34b2a4d9a406e6c80d3a5ba1d4dd64b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarioKartMadness::Application.config.session_store :cookie_store, key: '_mario_kart_madness_session'
