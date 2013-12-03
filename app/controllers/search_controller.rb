5912c3fc445bddd70f00ee52b31b22738b039cdc
Justin Dao <tjd5at@virginia.edu>
2013-12-02 20:38:32 -0500

    Updates search to contains

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,7 +11,7 @@ class SearchController < ApplicationController
     tracks = ActiveRecord::Base.connection.execute("
       SELECT * 
       FROM `tracks` 
-      WHERE tname LIKE '#{query}%'").to_a
+      WHERE tname LIKE '%#{query}%'").to_a
 
     tracks.each do |track|
       results.push([track[get_field_index(Track, "tname")],track_path(track[0])])
@@ -20,7 +20,7 @@ class SearchController < ApplicationController
     games = ActiveRecord::Base.connection.execute("
       SELECT * 
       FROM `games`
-      WHERE gname LIKE '#{query}%'").to_a
+      WHERE gname LIKE '%#{query}%'").to_a
 
     games.each do |game|
       results.push([game[get_field_index(Game, "gname")],game_path(game[0])])
@@ -29,7 +29,7 @@ class SearchController < ApplicationController
     consoles = ActiveRecord::Base.connection.execute("
       SELECT * 
       FROM `consoles`
-      WHERE gcname LIKE '#{query}%'").to_a
+      WHERE gcname LIKE '%#{query}%'").to_a
 
     consoles.each do |console|
       results.push([console[get_field_index(Console, "gcname")],console_path(console[0])])
@@ -38,7 +38,7 @@ class SearchController < ApplicationController
     items = ActiveRecord::Base.connection.execute("
       SELECT * 
       FROM `items`
-      WHERE iname LIKE '#{query}%'").to_a
+      WHERE iname LIKE '%#{query}%'").to_a
 
     items.each do |item|
       results.push([item[get_field_index(Item, "iname")],item_path(item[0])])
@@ -47,7 +47,7 @@ class SearchController < ApplicationController
     characters = ActiveRecord::Base.connection.execute("
       SELECT * 
       FROM `characters`
-      WHERE cname LIKE '#{query}%'").to_a
+      WHERE cname LIKE '%#{query}%'").to_a
 
     characters.each do |character|
       results.push([character[get_field_index(Character, "cname")],character_path(character[0])])
